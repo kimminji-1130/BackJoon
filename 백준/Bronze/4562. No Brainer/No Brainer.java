f636542e4b3d4bbdfa21e9ae2a597c9c8dca667d
김민지 <84611998+kimminji-1130@users.noreply.github.com>
2023-01-04 11:08:51 +0900

    [Bronze III] Title: No Brainer, Time: 204 ms, Memory: 17580 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.java
@@ -0,0 +1,16 @@
+import java.util.*;
+	public class Main {
+	public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    for(int i=0;i<num;i++){
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      if(b>a){
+        System.out.println("NO BRAINS");
+      }else{
+        System.out.println("MMM BRAINS");
+      }
+    }
+    }
+  }
