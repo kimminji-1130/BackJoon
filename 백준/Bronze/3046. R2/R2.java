20556215a1f3bff4c4894845da76d2fcaef9c1a8
김민지 <84611998+kimminji-1130@users.noreply.github.com>
2023-01-05 10:56:59 +0900

    [Bronze IV] Title: R2, Time: 212 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+	public class Main {
+	public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int r1 = sc.nextInt();
+    int s = sc.nextInt();
+    int r2 = s*2-r1;
+    System.out.print(r2);
+    
+    }
+  }
