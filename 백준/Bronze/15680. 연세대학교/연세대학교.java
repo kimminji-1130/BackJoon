27c716bd3903266e2b2651bcde7223665059cfd2
김민지 <84611998+kimminji-1130@users.noreply.github.com>
2023-01-06 14:06:49 +0900

    [Bronze IV] Title: 연세대학교, Time: 200 ms, Memory: 17548 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+     int a = sc.nextInt();
+     if(a == 0){
+       System.out.print("YONSEI");
+     }else{
+       System.out.print("Leading the Way to the Future");
+     }
+    }
+  }
