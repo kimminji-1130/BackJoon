f1e30688fd1b3f7fc269c92f49d1488ee4f8d017
김민지 <84611998+kimminji-1130@users.noreply.github.com>
2023-01-10 16:09:33 +0900

    [Bronze III] Title: FBI, Time: 156 ms, Memory: 16276 KB -BaekjoonHub

---
+++ 백준/Bronze/2857. FBI/FBI.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = "";
+		for (int i = 1; i <= 5; i++) {
+			if (br.readLine().contains("FBI"))
+				str += i + " ";
+		}
+		System.out.println(str != "" ? str.trim() : "HE GOT AWAY!");
+	}
+}
