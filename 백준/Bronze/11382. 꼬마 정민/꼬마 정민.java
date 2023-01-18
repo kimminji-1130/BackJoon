5cc1919de806ec3fd51432755097d9b47ab3260d
김민지 <84611998+kimminji-1130@users.noreply.github.com>
2023-01-18 16:41:37 +0900

    [Bronze V] Title: 꼬마 정민, Time: 220 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException{
+		Scanner sc = new Scanner(System.in);
+		
+		long a = sc.nextLong();
+		long b = sc.nextLong();
+		long c = sc.nextLong();
+ 
+ 
+		System.out.println(a+b+c);
+		
+	}
+ 
+}
