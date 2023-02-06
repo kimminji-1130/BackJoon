109572a36d086164038b9607fcb586f4389ef4e0
김민지 <84611998+kimminji-1130@users.noreply.github.com>
2023-02-06 10:04:34 +0900

    [Bronze V] Title: Multiply, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/22193. Multiply/Multiply.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Main {  
+  public static void main(String args[]) { 
+    Scanner sc = new Scanner(System.in);
+   int a = sc.nextInt();
+   int b = sc.nextInt();
+   int n = sc.nextInt();
+   int m = sc.nextInt();
+   System.out.print(n*m);
+  } 
+}
