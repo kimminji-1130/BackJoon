039c3001204dcfa8794ba499535abb6b868c8aaf
김민지 <84611998+kimminji-1130@users.noreply.github.com>
2023-02-08 12:00:14 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 220 ms, Memory: 17924 KB -BaekjoonHub

---
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.math.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger n = sc.nextBigInteger();
+        BigInteger m = sc.nextBigInteger();
+        sc.close();
+        System.out.println(n.divide(m));
+        System.out.println(n.remainder(m));
+
+	}
+}
