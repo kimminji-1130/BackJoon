f71717897675ba3f62b496c87e90056140788c83
김민지 <84611998+kimminji-1130@users.noreply.github.com>
2023-02-23 15:22:35 +0900

    [Bronze V] Title: A+B - 8, Time: 276 ms, Memory: 18984 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+
+		Scanner in = new Scanner(System.in);
+
+		int a = in.nextInt();
+
+		for (int i = 1; i <= a; i++) {
+			int c = in.nextInt();
+			int d = in.nextInt();
+
+			System.out.println("Case #" + i + ": " + c + " + " + d +" = "+(c + d));
+		}
+
+		in.close();
+	}
+}
